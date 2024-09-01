5b2ef8e6a0da2d9fcbb3d6a44cb79e0da9943c64
Oliver Searle-Barnes <oliver@opsb.co.uk>
2024-09-01 21:27:02 +0200

    Without sqlite bin finder

--- test/powersync_segfault_test.dart
+++ test/powersync_segfault_test.dart
@@ -1,4 +1,3 @@
-import 'dart:ffi';
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
@@ -6,8 +5,6 @@ import 'package:powersync/powersync.dart';
 import 'package:powersync/src/database/native/native_powersync_database.dart';
 import 'package:path/path.dart';
 import 'package:powersync/sqlite_async.dart';
-import 'package:sqlite3/common.dart';
-import 'package:sqlite3/open.dart' as sqlite_open;
 
 final sqliteBinPath = const String.fromEnvironment("SQLITE_BIN").trim();
 
@@ -23,27 +20,12 @@ const schema = Schema([
   ]),
 ]);
 
-class TestOpenFactory extends PowerSyncOpenFactory {
-  TestOpenFactory({required super.path});
-
-  @override
-  CommonDatabase open(SqliteOpenOptions options) {
-    if (sqliteBinPath.isEmpty) {
-      throw "SQLITE_BIN must be set and non-blank";
-    }
-
-    sqlite_open.open.overrideForAll(() => DynamicLibrary.open(sqliteBinPath));
-    return super.open(options);
-  }
-}
-
 void main() {
   group("debug powersync", () {
     test("teardown powersync", () async {
       final basepath = await Directory.systemTemp.createTemp("example");
       final dbPath = join(basepath.path, 'test_database.db');
-      final openFactory = TestOpenFactory(path: dbPath);
-      final db = SqliteDatabase.withFactory(openFactory);
+      final db = SqliteDatabase(path: dbPath);
       final powersyncDb = PowerSyncDatabaseImpl.withDatabase(schema: schema, database: db);
 
       print("tearing down");
